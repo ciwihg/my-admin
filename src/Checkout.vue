b2f02cdb5496cfa4a770570c381b8d2784012f0b
ciwi <ciwihg@gmail.com>
2020-07-21 17:43:22 +0800

    7.21

--- src/Checkout.vue
+++ src/Checkout.vue
@@ -1,5 +1,10 @@
 <template>
 <div class="m-list-wrap">
+  <div class="m-mode-bar" @change="handlemodeswitch">
+    <el-radio v-model="displaymode" label="recommand">自动推荐</el-radio>
+    <el-radio v-model="displaymode" label="search">搜索</el-radio>
+  </div>
+    <Filterbar :handler="filterdata" v-if="displaymode=='search'"></Filterbar>
   <el-table
   size="medium"
     :data="tableData"
@@ -30,12 +35,18 @@
 
 <script>
 import axios from 'axios';
+import Filterbar from './component/filterbar.vue';
 axios.defaults.baseURL = `https://easyhome.applinzi.com/public/index.php/ciwirent`;
 export default {
+  components:{
+    'Filterbar':Filterbar,
+  },
 data () {
   return {
     title:"抄表及打印",
-    tableData:[]
+    tableData:[],
+    displaymode:'recommand',
+    Alldatas:[],
   }
 },
 created:function(){
@@ -43,6 +54,32 @@ created:function(){
   this.$emit('update:title',this.title);
   this.$parent.$refs.drawer&&this.$parent.$refs.drawer.closeDrawer();
   axios.get("/checkoutpage").then(function(res){
+    vm.Alldatas=res.data;
+    /*let today=new Date(Date.now());
+    var day1=24*60*60*1000;
+    let odate=[];
+    odate.push(today.getDate());
+    for(let i=1;i<=3;i++){
+      let today=Date.now();
+      let dateobj=new Date(today-i*day1);
+      odate.push(dateobj.getDate());
+      dateobj=new Date(today+i*day1);
+      odate.push(dateobj.getDate());
+    }
+    vm.tableData = res.data.filter(i=>
+      odate.includes(new Date(i.date).getDate())
+);*/
+vm.autorecommand();
+  }).catch(function (err) {
+    console.log(err);
+  });
+
+},
+methods:{
+  handlerowclick:function (row) {
+    this.$router.push({name:'Inputprint',params:{hid:row.id}});
+  },
+  autorecommand:function () {
     let today=new Date(Date.now());
     var day1=24*60*60*1000;
     let odate=[];
@@ -54,25 +91,37 @@ created:function(){
       dateobj=new Date(today+i*day1);
       odate.push(dateobj.getDate());
     }
-    /*vm.tableData = res.data.filter(i=>
+    this.tableData = this.Alldatas.filter(i=>
       odate.includes(new Date(i.date).getDate())
-);*/
-console.log(res.data);
-    vm.tableData = res.data;
-  }).catch(function (err) {
-    console.log(err);
-  });
+);
+  },
+  filterdata:function (conditions) {
+   this.tableData=this.Alldatas.filter((i)=>{
+     if(i.number==conditions.number&&i.address==conditions.address){
+       return true;
+     }else{
+       return false;
+     }
+   })
+ },
+ handlemodeswitch:function () {
 
-},
-methods:{
-  handlerowclick:function (row) {
-    this.$router.push({name:'Inputprint',params:{hid:row.id}});
-  }
+   if(this.displaymode=="recommand"){
+     this.autorecommand();
+   }else{
+     this.tableData=[];
+   }
+ }
 }
 }
 </script>
 
 <style scoped>
+.m-mode-bar{
+  font-size: 0;
+  padding: .2rem .25rem;
+  background-color:white;
+}
 .m-table-style{
   width: 1.18rem;
 }
